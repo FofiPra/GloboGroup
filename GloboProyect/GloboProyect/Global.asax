16781407234990e1b605a504be7d108519bff3de
Fofi Pra <Fofi Pra@DESKTOP-HIITR9K>
2018-06-01 10:05:17 -0300

    Primera Prueba

---
+++ GloboProyect/GloboProyect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GloboProyect.MvcApplication" Language="C#" %>
---
+++ GloboProyect/GloboProyect/Models/Proyecto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GloboProyect.Models
+{
+    public class Proyecto
+    {
+        public int idCliente { get; set; }
+        public int idTipoDeProyecto { get; set; }
+        public string sitio { get; set; }
+        public string nombreDireccion { get; set; }
+        public int numeroDireccion { get; set; }
+        public int idLiderProyecto { get; set; }
+        public DateTime fechaInicioEstimada { get; set; }
+    }
+}
